5046885ecf65a09486c6e1a493284abc474835c0
msyildirim <msy.90@hotmail.com>
2022-10-24 22:34:45 +0300

    library simualtion with js

---
+++ library.js
@@ -0,0 +1,51 @@
+let myLibrary = [];
+
+function Book(bookName, author, category, page, readStatus){
+    this.bookName = bookName;
+    this.author = author;
+    this.page = page;
+    this.readStatus = readStatus;
+    this.category = category;
+}
+
+function addBookToLibrary(bookName, author, category, page, readStatus){
+    let book = new Book(bookName, author, category, page, readStatus);
+    myLibrary.push(book);
+}
+
+//Sidebar menu
+document.querySelectorAll(".sideBarMenu").forEach((menu)=>{
+    menu.addEventListener("click", (e)=>{
+        e.target.nextElementSibling.classList.toggle('show');
+    })
+})
+
+const addBookForm = document.forms.addBookForm;
+addBookForm.onsubmit = function(event){
+    let bookName = addBookForm.bookName.value;
+    console.log(bookName);
+    let author = addBookForm.author.value;
+    let bookPage = addBookForm.bookPage.value;
+    let readStatus = addBookForm.bookReadStatus.value;
+    let category = "unknonwn" //todo: add category info to html
+    let book = new Book(bookName, author, category, bookPage, readStatus);
+    displayBook(book);
+    addBookToLibrary(bookName, author, category, bookPage, readStatus);
+    event.preventDefault();
+}
+
+function displayBook(book){
+    const content = document.querySelector("#content");
+    let bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    bookElement.innerHTML = `${book.bookName} 
+    ${book.author}
+    ${book.page}`;
+    content.append(bookElement);
+};
+
+/* function displayAllBooks(){
+    myLibrary.forEach((book)=>{
+        displayBook(book);
+    })
+} */
